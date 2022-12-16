45185a2ccb5577f0f3f84b2360357762ce8afeb7
ssshun3 <s.kudo.22.7.584@4nonome.com>
2022-12-16 19:14:16 +0900

    taskの追加機能、削除機能の追加

--- src/components/Organisms/TodoCard/index.jsx
+++ src/components/Organisms/TodoCard/index.jsx
@@ -3,17 +3,52 @@ import styled from "styled-components";
 import Task from "../../Molecules/Task";
 import AddTaskButton from "../../Atoms/AddTaskButton";
 import COLOR from "../../../variables/color";
-import TEXT from "../../../variables/texts";
 const TodoCard = () => {
+  const [taskList, setTaskList] = useState([]);
+  const onAddTaskButtonClick = () => {
+    const todoTask = {
+      name: "",
+      initializing: true,
+    };
+    setTaskList(taskList.concat(todoTask));
+  };
+  const onTaskComplete = (index) => {
+    const deletedTodoList = [...taskList];
+    deletedTodoList.splice(index, 1);
+    setTaskList(deletedTodoList);
+  };
+  const onTaskNameChange = (value, index) => {};
   return (
     <StyledWrapper>
-      <AddTaskButton />
+      <AddTaskButton onClick={onAddTaskButtonClick} />
       <StyledTaskList>
-        <Task />
+        {taskList.map((task, index) => (
+          <Task
+            key={index}
+            onTaskComplete={(index) => onTaskComplete(index)}
+            onTaskNameChange={(value) => onTaskNameChange(value, index)}
+            taskName={task.name}
+            defaultIsEditing={task.initializing}
+          />
+        ))}
       </StyledTaskList>
     </StyledWrapper>
   );
 };
 export default TodoCard;
-const StyledWrapper = styled.div``;
-const StyledTaskList = styled.div``;
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  background-color: ${COLOR.LIGHT_BLACK};
+  border-radius: 4px;
+  padding: 20px;
+  gap: 10px;
+`;
+const StyledTaskList = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2px 6px;
+  gap: 10px;
+  align-self: stretch;
+`;
