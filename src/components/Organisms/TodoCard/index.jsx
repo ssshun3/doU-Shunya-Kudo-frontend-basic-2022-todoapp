0ed2f179cfa89264ce91e618b484f537f461c57c
ssshun3 <s.kudo.22.7.584@4nonome.com>
2022-12-17 16:44:32 +0900

    Local Storageの使用

--- src/components/Organisms/TodoCard/index.jsx
+++ src/components/Organisms/TodoCard/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Task from "../../Molecules/Task";
 import AddTaskButton from "../../Atoms/AddTaskButton";
@@ -25,9 +25,16 @@ const TodoCard = () => {
     } else {
       const newTaskList = [...taskList];
       newTaskList[index].name = value;
+      newTaskList[index].initializing = false;
       setTaskList(newTaskList);
     }
   };
+  useEffect(() => {
+    setTaskList(JSON.parse(window.localStorage.getItem("taskList")));
+  }, []);
+  useEffect(() => {
+    window.localStorage.setItem("taskList", JSON.stringify(taskList));
+  }, [taskList]);
   return (
     <StyledWrapper>
       <AddTaskButton onClick={onAddTaskButtonClick} />
