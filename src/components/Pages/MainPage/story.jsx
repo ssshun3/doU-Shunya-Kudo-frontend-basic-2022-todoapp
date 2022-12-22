8cb4568bda91f90b451728fbee93a9954c2527e4
ssshun3 <s.kudo.22.7.584@4nonome.com>
2022-12-22 19:41:59 +0900

    storybookにalertを追加する

--- src/components/Pages/MainPage/story.jsx
+++ src/components/Pages/MainPage/story.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Component from "./index";
-
+import { AlertHandlerProvider } from "../../../contexts/alert_handler";
+import AlertManager from "../../Organisms/AlertManager";
 export default {
   component: Component,
   title: "Pages/MainPage",
@@ -11,7 +12,12 @@ export default {
   },
 };
 
-const Template = (args) => <Component {...args} />;
+const Template = (args) => (
+  <AlertHandlerProvider>
+    <Component {...args} />
+    <AlertManager />
+  </AlertHandlerProvider>
+);
 
 export const Default = Template.bind({});
 Default.args = {};
