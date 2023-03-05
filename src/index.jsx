6bfb40dbbce0b495c04977637b076e45a1b83de9
ssshun3 <s.kudo.22.7.584@4nonome.com>
2023-03-05 23:37:55 +0900

    pageの実験

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import MainPage from "./components/Pages/MainPage";
 import "./style.css";
-import { AlertHandlerProvider } from "./contexts/alert_handler";
-import AlertManager from "./components/Organisms/AlertManager";
 
-ReactDOM.render(
-  <AlertHandlerProvider>
-    <AlertManager />
-    <MainPage />
-  </AlertHandlerProvider>,
-  document.getElementById("app")
-);
+ReactDOM.render(<MainPage />, document.getElementById("app"));
